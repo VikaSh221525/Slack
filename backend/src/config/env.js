3d9bef92ba4fb1f680eaf59670bd847ca213c56f
VikaSh221525 <vikassharma221525@gmail.com>
2025-09-04 21:18:42 +0530

    DbConnected&env.jsCompleted

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,5 +3,11 @@ export const ENV = {
     PORT : process.env.PORT,
     DB_URL : process.env.DB_URL,
     NODE_ENV : process.env.NODE_ENV,
-    JWT_SECRET : process.env.JWT_SECRET
+    CLERK_PUBLISHABLE_KEY:  process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY
 }
